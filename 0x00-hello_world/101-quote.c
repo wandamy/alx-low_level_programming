78d0b18ee55d70bb03fe80f476ed1fc8a23906a1
wandamy <amywanderi1@gmail.com>
2022-06-02 15:47:12 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void){
+char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, c, 59);
+return (1);
+}
