d2b7b40be84ed1076e524a08fa3f4fb9c0d230c0
wandamy <amywanderi1@gmail.com>
2022-06-02 10:21:41 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,5 @@ printf("size of an int: %zu byte(s)\n", sizeof(inttype));
 printf("size of a long int: %zu byte(s)\n", sizeof(longinttype));
 printf("size of a long long int: %zu byte(s)\n", sizeof(longlongint));
 printf("size of a float: %zu byte(s)\n", sizeof(floattype));
-return 0;
+return (0);
 }
