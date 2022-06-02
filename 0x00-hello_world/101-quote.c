440ba5f3c67a2e5205a6e7232b90069d4fedbe55
wandamy <amywanderi1@gmail.com>
2022-06-02 15:48:37 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 1 (Success)
  */
-int main(void){
+int main(void)
+{
 char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, c, 59);
 return (1);
