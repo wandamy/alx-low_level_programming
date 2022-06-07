2a25e239ab51b637212039e1dfda2fdb67d79590
wandamy <amywanderi1@gmail.com>
2022-06-07 11:23:10 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,14 @@
  */
 int main(void)
 {
-write(1, "_putchar", 57);
-write(1, "\n", 57);
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+_putchar('\n');
 return (0);
 }
