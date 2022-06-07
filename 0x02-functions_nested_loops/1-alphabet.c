8670b781a6d6968f293ca540b9ec3b96e630338c
wandamy <amywanderi1@gmail.com>
2022-06-07 12:09:10 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_putchar(97);
+_putchar(98);
+_putchar(99);
+_putchar(100);
+_putchar(101);
+_putchar(102);
+_putchar(103);
+_putchar(104);
+_putchar(105);
+_putchar(106);
+_putchar(107);
+_putchar(108);
+_putchar(109);
+_putchar(110);
+_putchar(111);
+_putchar(112);
+_putchar(113);
+_putchar(114);
+_putchar(115);
+_putchar(116);
+_putchar(117);
+_putchar(118);
+_putchar(119);
+_putchar(120);
+_putchar(121);
+_putchar(122);
+return (0);
+}
