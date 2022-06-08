70efaed3105c8406488155c4f60a6b11740dcd6d
wandamy <amywanderi1@gmail.com>
2022-06-08 15:53:02 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - checks if c is a letter, lowercase or uppercase
+ * @c: character argument
+ * Return: 1 or 0 depending on condition
+ */
+int _isalpha(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
