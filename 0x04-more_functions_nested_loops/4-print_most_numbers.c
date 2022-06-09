1fb529df8dd2aacc229daa66c46c62cc127b061f
wandamy <amywanderi1@gmail.com>
2022-06-09 15:11:33 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,15 @@
  */
 void print_most_numbers(void)
 {
-int c = '0';
-for (c = '0'; c <= '9'; c++)
+int c = 0;
+for (c = 0; c <= 9; c++)
 {
 if (c == 2 || c == 4)
 {
 }
 else
 {
-_putchar(c + '0');
+_putchar(c);
 }
 }
 _putchar('\n');
