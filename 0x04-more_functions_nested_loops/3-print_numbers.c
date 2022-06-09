50c1b2047b1e90c64738968c10404fc14ac07100
wandamy <amywanderi1@gmail.com>
2022-06-09 07:50:00 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers 0 to 9
+ */
+void print_numbers(void)
+{
+char c = '0';
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n')
+}
