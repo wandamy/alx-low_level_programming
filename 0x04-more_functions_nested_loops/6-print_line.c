3d54f289434fc36d451c3b182c304c34047dd4c3
wandamy <amywanderi1@gmail.com>
2022-06-09 15:45:31 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,21 @@
 /**
  * print_line - draws a straight line in the terminal
  * return: 0 success
+ * @n: the number of lines to draw
  */
 void print_line(void)
 {
-
+int c;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (c = 0; c < n; c++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+}
