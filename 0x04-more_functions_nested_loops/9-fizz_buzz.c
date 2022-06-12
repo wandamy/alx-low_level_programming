8bb15a98cb3ba6710344aaed866379b13b7b718e
wandamy <amywanderi1@gmail.com>
2022-06-12 05:52:41 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,9 +8,7 @@ int main(void)
 int c;
 for (c = 1; c <= 100; c++)
 {
-if (c == 100)
-printf("Buzz ");
-else if (c % 3 == 0)
+if (c % 3 == 0)
 printf("Fizz ");
 else if (c % 5 == 0)
 printf("Buzz ");
