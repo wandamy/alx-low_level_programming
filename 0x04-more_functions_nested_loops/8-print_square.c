602e28cc400b201d091405dd49f2634eee0e83b6
wandamy <amywanderi1@gmail.com>
2022-06-12 04:54:35 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: size of square
+ */
+void print_square(int size);
+{
+int c;
+int y;
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (c = 0; c < size; c++)
+{
+for (y = 0; y < size; y++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
