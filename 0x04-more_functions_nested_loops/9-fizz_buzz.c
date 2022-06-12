8c5059fe55b81e966d304fb378c72329da0225af
wandamy <amywanderi1@gmail.com>
2022-06-12 06:06:14 -0700

    fizzes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,21 @@ int c;
 for (c = 1; c < 101; c++)
 {
 if (c % 3 == 0)
+{
 printf("Fizz ");
+}
 else if (c % 5 == 0)
+{
 printf("Buzz ");
+}
 else if (c % 3 == 0 && c % 5 == 0)
+{
 printf("FizzBuzz ");
+}
 else
+{
 printf("%d ", c);
+}
 }
 printf("\n");
 return (0);
