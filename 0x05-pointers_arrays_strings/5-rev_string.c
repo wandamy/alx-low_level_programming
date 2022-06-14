5f9276723f5d4ebdcafb9397b4b502fb638aac83
wandamy <105356198+wandamy@users.noreply.github.com>
2022-06-15 00:44:13 +0300

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*rev_string - function that reverses a string
+*@s: pointer to function
+*/
+void rev_string(char *s)
+{
+int i, length1, length2;
+length1 = 0;
+length2 = 0;
+while (s[length1] != '\0')
+length1++;
+length2 = length1 - 1;
+for (i = 0; i < length1 / 2; i++)
+{
+"samalx" "xlamas"
+tmp = s[i];
+s[length2] = tmp;
+length2 -= 1;
+}
+}
