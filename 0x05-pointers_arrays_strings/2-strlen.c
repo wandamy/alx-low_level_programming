efa38af75d16f6efda16760458dbe784849f8b25
wandamy <105356198+wandamy@users.noreply.github.com>
2022-06-14 22:39:09 +0300

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strlen - function that returns the length ofa string
+*@s - character argument
+*/
+int _strlen(char *s)
+{
+char *s[17];
+strlen(*s);
+}
