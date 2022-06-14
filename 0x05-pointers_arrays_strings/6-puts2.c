7ee0f2178afdb2895cfaa239fdf6a40d02a18bb6
wandamy <105356198+wandamy@users.noreply.github.com>
2022-06-15 01:06:42 +0300

    o

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*puts2 - prints every other character of a string
+*@str: character argument
+*/
+void puts2(char *str)
+{
+int len, i;
+len = strlen(str);
+for (i = 0; i < len; i +=2)
+_putchar(str[i]);
+_putchar('\n');
+}
