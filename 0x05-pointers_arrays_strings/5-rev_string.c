c00b27e88aaaa404748996db638eadc2f48a9fe1
wandamy <105356198+wandamy@users.noreply.github.com>
2022-06-15 00:47:03 +0300

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,6 +5,7 @@
 */
 void rev_string(char *s)
 {
+char tmp;
 int i, length1, length2;
 length1 = 0;
 length2 = 0;
@@ -13,7 +14,6 @@ length1++;
 length2 = length1 - 1;
 for (i = 0; i < length1 / 2; i++)
 {
-"samalx" "xlamas"
 tmp = s[i];
 s[length2] = tmp;
 length2 -= 1;
