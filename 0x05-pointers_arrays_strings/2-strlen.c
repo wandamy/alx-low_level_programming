884d9f9c4c7f219b4fa809906a0078b084201cfd
wandamy <105356198+wandamy@users.noreply.github.com>
2022-06-14 23:34:28 +0300

    strlenn

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 #include <string.h>
 /**
 *_strlen - function that returns the length ofa string
-*@s - string pointer to passed to this function
+*@s: string pointer to passed to this function
 *Return: returns length of string
 */
 int _strlen(char *s)
