4ca2ab723fe6d85c12a4eca9c03063e90c7cda24
wandamy <105356198+wandamy@users.noreply.github.com>
2022-06-15 01:18:48 +0300

    half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,12 @@
 void puts_half(char *str)
 {
 int x, y, i;
-x =strlen(str);
+x = strlen(str);
 if (x % 2 == 1)
 y = x / 2 + 1;
 else
 y = x / 2;
-for (i = y; i < x; i ++)
+for (i = y; i < x; i++)
 _putchar(str[i]);
 _putchar('\n');
 }
