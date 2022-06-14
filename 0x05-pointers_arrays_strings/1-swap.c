ac6eedb760b7fad548c551e97fd68f03f09b78a5
wandamy <105356198+wandamy@users.noreply.github.com>
2022-06-14 20:00:07 +0300

    swapping again

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,11 @@
 /**
 * swap_int - swaps the value of two pointers
 *@a: character argument
-@b: character argument
+@b: parameter function
 */
 void swap_int(int *a, int *b)
 {
 int val;
-*a = 7;
-*b = 8;
 val = *a;
 *a = *b;
 *b = val;
