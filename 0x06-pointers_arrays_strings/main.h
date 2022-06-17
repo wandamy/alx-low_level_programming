d1264ec987fe7d5215452f58e853b77a89e61228
wandamy <105356198+wandamy@users.noreply.github.com>
2022-06-17 23:12:40 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-=======
+int _putchar(int c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
->>>>>>> fe1e3ff86e4abe68fc8f845e83d624731cf6e50e
+
