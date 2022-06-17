ed522f31bf03d458fb1353993bea589be7f5824d
wandamy <105356198+wandamy@users.noreply.github.com>
2022-06-17 16:15:27 +0300

    strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
 *_strcat - function that concentrates two strings
 *@dest: destination input
@@ -6,11 +7,14 @@
 */
 char _strcat(char *dest, char *src)
 {
-int c, c2;
-c = 0;
-while (dest[c])
-c++;
-for (c2 = 0; src[c2]; c2++)
-dest[c++] = src[c2];
-return 9dest0;
+int len = 0, i;
+while (dest[len])
+len++;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[len] = src[i];
+len += 1;
+}
+dest[len] = '\0';
+return (dest)
 }
