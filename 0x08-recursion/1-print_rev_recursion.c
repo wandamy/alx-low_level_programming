d3761718650e38dc7491d17e1bbe330cb85c0b94
wandamy <amywanderi1@gmail.com>
2022-06-23 11:52:46 -0700

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints string in recursion
+ * @s: string
+ * Return: string in reverse
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+return;
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
+}
