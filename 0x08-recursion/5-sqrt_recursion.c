2d7856f39903397434c614e64866594ae51f6c6a
wandamy <amywanderi1@gmail.com>
2022-06-23 14:06:24 -0700

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,8 +6,20 @@
  */
 int _sqrt_recursion(int n)
 {
-if (n % 2 != 0)
+return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - recursive square root
+ * @n: number
+ * @i: iterator
+ * Return: number
+ */
+int _sqrt(int n, int i)
+{
+int square = i * i;
+if (square > n)
 return (-1);
-else
-return (_sqrt(n));
+if (square == n)
+return (i);
+return (_sqrt(n, i + 1));
 }
