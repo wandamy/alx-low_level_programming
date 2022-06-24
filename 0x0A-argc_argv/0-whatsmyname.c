f0536b80416cf0b76562c90553ed6f70ccd96cad
wandamy <amywanderi1@gmail.com>
2022-06-24 15:49:50 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: program name
+ */
+int main(int argc, char* argv[])
+{
+return (argc);
+}
