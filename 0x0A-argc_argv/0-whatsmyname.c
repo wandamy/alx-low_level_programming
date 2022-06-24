277c2d4bcd8a95902a2ad38686560e141eecef09
wandamy <amywanderi1@gmail.com>
2022-06-24 16:13:56 -0700

    PROGRam name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,10 @@
  * main - function
  * @argc: argument count
  * @argv: argument vector
- * Return: program name
+ * Return: 0 for success
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 printf ("%s\n", *argv);
+return (0);
 }
