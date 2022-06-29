2e08d78ac54328fe2bbef724113a8fbbb87ca411
wandamy <amywanderi1@gmail.com>
2022-06-29 16:12:01 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - a function that prints it's name
+ * @argc: character argument
+ * @argv: character argument
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+int result = 0, num, i, j, k;
+if (argc == 1)
+{
+printf("0\n");
+for (i = 0; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > '9' || argv[i][j] < '0')
+{
+printf("%s\n", "Error");
+return (1);
+}
+}
+}
+for (k = 0; k < argc; k++)
+{
+num = atoi(argv[k]);
+result += num;
+}
+printf("%d\n", result);
+return (0);
+}
