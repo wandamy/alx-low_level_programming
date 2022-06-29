7004d1b2af155c7d10e44507e9afebc2b1277844
wandamy <amywanderi1@gmail.com>
2022-06-29 16:36:15 -0700

    mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - function
  * @argc: argument count
@@ -15,7 +16,7 @@ return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num 1 * num 2;
+result = num1 * num2;
 printf("%d\n", result);
 return (0);
 }
